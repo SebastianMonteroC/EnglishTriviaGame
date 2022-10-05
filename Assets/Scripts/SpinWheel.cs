ebaf602456b4fe947c552f826eb13315da54370a
Sebastian <sebasmc_2000@hotmail.com>
2022-10-05 14:53:59 -0600

    Question box added + timer

--- Assets/Scripts/SpinWheel.cs
+++ Assets/Scripts/SpinWheel.cs
@@ -6,60 +6,98 @@ using UnityEngine.UI;
 
 public class SpinWheel : MonoBehaviour
 {
-    [SerializeField] private Button SpinButtonUI;
-    [SerializeField] private Text SpinButtonText;
-    [SerializeField] private PickerWheel Wheel;
+    [SerializeField] private Button spinButtonUI;
+    [SerializeField] private Text spinButtonText;
+    [SerializeField] private PickerWheel wheel;
     
-    private string CurrentWheelPiece;
+    private string currentWheelPiece;
 
     //Play box displayed after spinning the wheel
-    private GameObject PlayBox;
-    private Text LearningAbility;
-    private Button PlayButton;
+    private GameObject playBox;
+    private Text learningAbility;
+    private Button playButton;
+
+    //question box displayed after pressing "play"
+    private GameObject questionBox;
+    private Text questionAbility;
+    private Text question;
+    private Text timer;
+    private float timerValue = 10;
+    private bool timerOn;
 
     private void Start(){
-        SpinButtonUI.onClick.AddListener(Spin);
-        PlayBox = GameObject.Find("ConfirmPlayBox");
+        spinButtonUI.onClick.AddListener(Spin);
+        playBox = GameObject.Find("ConfirmPlayBox");
+        playButton = GameObject.Find("PlayButton").GetComponent<Button>();
+        learningAbility = GameObject.Find("Theme").GetComponent<Text>();
+        
+        questionBox = GameObject.Find("QuestionBox");
+        questionAbility = GameObject.Find("Theme").GetComponent<Text>();
+        timer = GameObject.Find("Timer").GetComponent<Text>();
+        question = GameObject.Find("Question").GetComponent<Text>();
+        
+        GameObject.Find("ListeningImage").GetComponent<Image>().enabled = false;
+        GameObject.Find("SpeakingImage").GetComponent<Image>().enabled = false;
+        GameObject.Find("ReadingImage").GetComponent<Image>().enabled = false;
+        GameObject.Find("WritingImage").GetComponent<Image>().enabled = false;
 
-        LearningAbility = GameObject.Find("Theme").GetComponent<Text>();
+        playBox.SetActive(false);
+        questionBox.SetActive(false);
+    }
 
-        GameObject.Find("ListeningImage").GetComponent<Image>().enabled = false;;
-        GameObject.Find("SpeakingImage").GetComponent<Image>().enabled = false;;
-        GameObject.Find("ReadingImage").GetComponent<Image>().enabled = false;;
-        GameObject.Find("WritingImage").GetComponent<Image>().enabled = false;;
-
-        PlayButton = GameObject.Find("PlayButton").GetComponent<Button>();
-        PlayBox.SetActive(false);
+    private IEnumerator RunTimer(){
+        Color color = Color.black;
+        while (timerValue > 0){
+            yield return new WaitForSeconds(1f);
+            timerValue --;
+            if(timerValue < 7){
+                color = Color.yellow;
+            }
+            if(timerValue < 4){
+                color = Color.red;
+            }
+            ChangeTimerDisplay(color);
+        }
     }
 
     private void Spin(){
-        SpinButtonUI.interactable = false;
-        SpinButtonText.text = "Spinning";
+        spinButtonUI.interactable = false;
+        spinButtonText.text = "Spinning";
 
-        Wheel.OnSpinStart(() =>{
-            Debug.Log("Spinning Wheel!");
+        wheel.OnSpinStart(() =>{
+            Debug.Log("Spinning wheel!");
         });
 
-        Wheel.OnSpinEnd(wheelPiece => {
-            SpinButtonUI.interactable = true;
-            SpinButtonText.text = "Spin!";
+        wheel.OnSpinEnd(wheelPiece => {
             Debug.Log(wheelPiece.Label);
-            CurrentWheelPiece = wheelPiece.Label;
+            currentWheelPiece = wheelPiece.Label;
             ShowPlayScreen();
         });
-        Wheel.Spin();
+        wheel.Spin();
     }
 
     private void ShowPlayScreen(){
         Debug.Log("Reaching here");
-        PlayBox.SetActive(true);
-        LearningAbility.text = CurrentWheelPiece;
-        GameObject.Find(CurrentWheelPiece + "Image").GetComponent<Image>().enabled = true;
-        
+        playBox.SetActive(true);
+        learningAbility.text = currentWheelPiece;
+        GameObject.Find(currentWheelPiece + "Image").GetComponent<Image>().enabled = true;
     }
 
     public void BeginTurn(){
-        GameObject.Find(CurrentWheelPiece + "Image").GetComponent<Image>().enabled = false;
-        PlayBox.SetActive(false);
+        GameObject.Find(currentWheelPiece + "Image").GetComponent<Image>().enabled = false;
+        playBox.SetActive(false);
+        questionBox.SetActive(true);
+        timer.text = timerValue.ToString();
+        StartCoroutine(RunTimer());
+    }
+
+    private void FormulateQuestion(){
+
+    }
+
+    private void ChangeTimerDisplay(Color color){
+        timer.color = color;
+        timer.text = timerValue.ToString();
     }
 }
+
