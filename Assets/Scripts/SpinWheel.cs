9468ba5e19e5df7489585a8251c3192106b4c81e
Sebastian <sebasmc_2000@hotmail.com>
2022-10-05 12:52:23 -0600

    play screen added when wheel stops turning + sound effects added

--- Assets/Scripts/SpinWheel.cs
+++ Assets/Scripts/SpinWheel.cs
@@ -9,10 +9,29 @@ public class SpinWheel : MonoBehaviour
     [SerializeField] private Button SpinButtonUI;
     [SerializeField] private Text SpinButtonText;
     [SerializeField] private PickerWheel Wheel;
+    
+    private string CurrentWheelPiece;
+
+    //Play box displayed after spinning the wheel
+    private GameObject PlayBox;
+    private Text LearningAbility;
+    private Button PlayButton;
 
     private void Start(){
         SpinButtonUI.onClick.AddListener(Spin);
+        PlayBox = GameObject.Find("ConfirmPlayBox");
+
+        LearningAbility = GameObject.Find("Theme").GetComponent<Text>();
+
+        GameObject.Find("ListeningImage").GetComponent<Image>().enabled = false;;
+        GameObject.Find("SpeakingImage").GetComponent<Image>().enabled = false;;
+        GameObject.Find("ReadingImage").GetComponent<Image>().enabled = false;;
+        GameObject.Find("WritingImage").GetComponent<Image>().enabled = false;;
+
+        PlayButton = GameObject.Find("PlayButton").GetComponent<Button>();
+        PlayBox.SetActive(false);
     }
+
     private void Spin(){
         SpinButtonUI.interactable = false;
         SpinButtonText.text = "Spinning";
@@ -25,7 +44,22 @@ public class SpinWheel : MonoBehaviour
             SpinButtonUI.interactable = true;
             SpinButtonText.text = "Spin!";
             Debug.Log(wheelPiece.Label);
+            CurrentWheelPiece = wheelPiece.Label;
+            ShowPlayScreen();
         });
         Wheel.Spin();
+    }
+
+    private void ShowPlayScreen(){
+        Debug.Log("Reaching here");
+        PlayBox.SetActive(true);
+        LearningAbility.text = CurrentWheelPiece;
+        GameObject.Find(CurrentWheelPiece + "Image").GetComponent<Image>().enabled = true;
+        
+    }
+
+    public void BeginTurn(){
+        GameObject.Find(CurrentWheelPiece + "Image").GetComponent<Image>().enabled = false;
+        PlayBox.SetActive(false);
     }
 }
